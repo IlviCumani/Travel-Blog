ef16be34b951e770d288310a30bf8af250e91a11
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-08-07 13:54:08 +0200

    changing some style files in the homepage

--- Controlls/header-script.js
+++ Controlls/header-script.js
@@ -1,9 +1,11 @@
 const toggleButton = document.getElementsByClassName('toggle-buttonn')[0];
 const elements = document.getElementsByClassName('elements')[0];
+const selectedLang = document.getElementsByClassName('lang-menuu')[0];
 let elementsActive = false;
 toggleButton.addEventListener('click', (event) =>{
   event.preventDefault();
   elements.classList.toggle('active');
+  selectedLang.classList.toggle('active');
   elementsActive = !elementsActive;
   if(elementsActive)
     document.getElementsByClassName('titlee')[0].style.marginTop = "290px";
