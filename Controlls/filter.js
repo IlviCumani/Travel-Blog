81a1a64edacbc985aaf6221ea5c302216e0e30c8
AmaraCela <amaracela@gmail.com>
2023-08-10 23:16:10 +0200

    new page created and styled

--- Controlls/filter.js
+++ Controlls/filter.js
@@ -3,7 +3,18 @@ let x = document.getElementById("closing-x");
 
 let price = document.querySelector("input[id='price']");
 let numDays = document.getElementById("numberOfDays");
-let priceForm = document.querySelector("form[class='price-form']");
+let priceForm = document.getElementsByClassName("price-form")[0];
+
+let range = document.querySelector("input[type='range']");
+let value = document.querySelector("input[type='number']");
+
+range.addEventListener("change",function(){
+value.value = range.value;
+});
+
+value.addEventListener("change",function(){
+range.value=value.value;
+});
 
 filterIcon.addEventListener("click",function(){
     document.getElementById("filter-div").style.display="inline-flex";
@@ -14,7 +25,7 @@ x.addEventListener("click",function(){
 });
 
 price.addEventListener("click",function(){
-togglePrice();
+    togglePrice();
 });
 
 numDays.addEventListener("click",function(){
@@ -23,8 +34,8 @@ numDays.addEventListener("click",function(){
 
 function togglePrice()
 {
-    if(priceForm.style.display=="none")
-    {
+    if(priceForm.style.display==="none" || priceForm.style.display==="")
+    { 
         priceForm.style.display = "inline-flex";
     }
     else{
