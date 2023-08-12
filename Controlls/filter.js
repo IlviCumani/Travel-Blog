1f25869135e3d70fd2735c866781c71149a644a2
AmaraCela <amaracela@gmail.com>
2023-08-12 11:31:46 +0200

    filter made functional, back of the cards stylised

--- Controlls/filter.js
+++ Controlls/filter.js
@@ -12,18 +12,40 @@ let priceSelection = document.getElementById("maxPrice");
 let daysSelection = document.getElementById("p-numDays");
 let select = document.getElementById("numDays"); 
 
+let filterBtn = document.getElementById("filter-btn");
+let removeFilter = document.getElementById("remove-filter");
+let tours =  document.getElementsByClassName("card");
+
+let priceChanged = false;
+let daysChanged = false;
+
+
 range.addEventListener("change",function(){
 value.value = range.value;
-priceSelection.innerText = "Max price : " + range.value;
+priceSelection.innerText = "Max price : " + range.value+" $";
+filterBtn.removeAttribute("disabled");
+filterBtn.classList.add("active");  
+priceChanged = true;
 });
 
 value.addEventListener("change",function(){
 range.value=value.value;
 priceSelection.innerText = "Max price : " + value.value +" $";
+filterBtn.removeAttribute("disabled");
+filterBtn.classList.add("active");  
+priceChanged = true;
 });
 
 filterIcon.addEventListener("click",function(){
-    document.getElementById("filter-div").style.display="inline-flex";
+    if(document.getElementById("filter-div").style.display=="none")
+    {
+         document.getElementById("filter-div").style.display="inline-flex";
+    }
+    else
+    {
+        document.getElementById("filter-div").style.display="none";
+    }
+   
 });
 
 x.addEventListener("click",function(){
@@ -42,7 +64,10 @@ numDays.addEventListener("click",function(){
 });
     
 select.addEventListener("change",function(){
- daysSelection.innerText = "Num of days: " + select.value;   
+ daysSelection.innerText = "Num of days: " + select.value; 
+ filterBtn.removeAttribute("disabled");
+ filterBtn.classList.add("active");  
+ daysChanged = true;
 });
   
 
@@ -66,3 +91,62 @@ function toggleDays(){
         daysForm.style.display="none";
     }
 }
+
+//when the button is pressed the cards displayed are filtered
+filterBtn.addEventListener("click",function(){
+    removeFilter.removeAttribute("disabled");
+    removeFilter.classList.add("active");
+    let maxDays;
+    let maxPrice;
+    if(daysChanged)
+    {
+        maxDays = parseInt(select.value);
+    }
+    if(priceChanged)
+    {
+        maxPrice = parseInt(value.value);
+    }
+
+    for(let card of tours)
+    {
+        if(maxPrice)
+        {
+             if(parseInt(card.querySelector(".price").id)>parseInt(maxPrice))
+             {
+                card.style.display="none";
+                continue;
+             }
+             else if(card.style.display=="none")
+             {
+                card.style.display="flex";
+             }
+        }
+        if(maxDays)
+        {
+            if(parseInt(card.querySelector(".tag").id)!=maxDays)
+            {
+                card.style.display="none";
+            }
+            else if(card.style.display=="none")
+            {
+               card.style.display="flex";
+            }
+        }
+       
+    }
+});
+removeFilter.addEventListener("click",function(){
+    for(let card of tours)
+    {
+        card.style.display="flex";
+    }
+});
+
+
+for(let card of tours)
+    {
+        card.querySelector(".back-card").addEventListener("click",function(){
+            window.location = "../Views/individualTour.html";
+        })
+
+    }
