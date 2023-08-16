3a35f1e7c9eb46035b7d9c2d0e10a05285fe84dd
AmaraCela <amaracela@gmail.com>
2023-08-16 10:17:18 +0200

    carousel added

--- Controlls/ourworkCarousel.js
+++ Controlls/ourworkCarousel.js
@@ -1,6 +1,7 @@
 let workCards = document.getElementsByClassName("carousel-card");
 let arrowLeft = document.querySelector("img[alt='left-arrow']");
 let arrowRight = document.querySelector("img[alt='right-arrow']");
+let activeLeft = 0;
 
 for(let i=0;i<workCards.length;i++)
 {
@@ -11,6 +12,9 @@ for(let i=0;i<workCards.length;i++)
     workCards[i].style.display="none";
 }
 
-arrowLeft.addEventListener("click",function(){
-    
+arrowRight.addEventListener("click",function(){
+    workCards[activeLeft].style.display = "none";
+    workCards[(activeLeft+3)%workCards.length].style.display="flex";
+    activeLeft = (activeLeft+1)%workCards.length;
 });
+
