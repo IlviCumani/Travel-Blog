49722d2530764296c91c30967b722356cb16b5bf
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-08-21 22:29:03 +0200

    popup page linked to the index page Scrolled effect added

--- Controlls/load_threeInOne.js
+++ Controlls/load_threeInOne.js
@@ -1,18 +1,46 @@
+// $(document).ready(function() {
+//   // const popUpBtn = $('.');
+//   const popUpContainer = $('#threeInOne-popup-container');
+//   // console.log(popUpBtn);
+//   console.log(popUpContainer);
+//   let popupShown = false;
+//   $('.popup-button').click(function(){
+//     if(!popupShown){
+//       $.get('../Views/threeInOnePage.html', function(data){
+//         popUpContainer.html(data);
+//         popupShown = true;
+//         $('#threeinone').removeClass('inactive-popup');
+//       })
+//     }else{
+//       $('#threeinone').toggleClass('inactive-popup');
+//     }
+//   })
+// });
 $(document).ready(function() {
-  const popUpBtn = $('.shortPopUp');
-  const popUpContainer = $('#co');
-  console.log(popUpBtn);
-  console.log(popUpContainer);
+  const popUpContainer = $('#threeInOne-popup-container');
   let popupShown = false;
-  popUpBtn.click(function(){
+
+  $('.popup-button').click(function(){
     if(!popupShown){
-      $.get('/Views/footer.html', function(data){
+      $.get('../Views/threeInOnePage.html', function(data){
         popUpContainer.html(data);
         popupShown = true;
         $('#threeinone').removeClass('inactive-popup');
-      })
+
+        // Scroll to the desired section based on the button clicked
+        const buttonId = $(this).attr('id');
+        const sectionId = `${buttonId}-section`;
+        scrollToSection(sectionId);
+      }.bind(this)); // Bind the click context to the callback function
     }else{
       $('#threeinone').toggleClass('inactive-popup');
     }
-  })
-})
+  });
+
+  // Function to scroll to a section by its ID
+  function scrollToSection(sectionId) {
+    $('html, body').animate({
+      scrollTop: $(`#${sectionId}`).offset().top
+    }, 500); // Adjust the scrolling speed as needed
+  }
+});
