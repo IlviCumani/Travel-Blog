12a109b930c964b985b97f307a5bd7d6b0a37a57
Anita Mjeshtri <122668935+AnitaMjeshtri@users.noreply.github.com>
2023-08-22 16:32:36 +0200

    popup option added to every page

--- Controlls/load_threeInOne.js
+++ Controlls/load_threeInOne.js
@@ -1,46 +1,46 @@
-// $(document).ready(function() {
-//   // const popUpBtn = $('.');
-//   const popUpContainer = $('#threeInOne-popup-container');
-//   // console.log(popUpBtn);
-//   console.log(popUpContainer);
-//   let popupShown = false;
-//   $('.popup-button').click(function(){
-//     if(!popupShown){
-//       $.get('../Views/threeInOnePage.html', function(data){
-//         popUpContainer.html(data);
-//         popupShown = true;
-//         $('#threeinone').removeClass('inactive-popup');
-//       })
-//     }else{
-//       $('#threeinone').toggleClass('inactive-popup');
-//     }
-//   })
-// });
 $(document).ready(function() {
   const popUpContainer = $('#threeInOne-popup-container');
+  console.log(popUpContainer);
   let popupShown = false;
-
   $('.popup-button').click(function(){
     if(!popupShown){
       $.get('../Views/threeInOnePage.html', function(data){
         popUpContainer.html(data);
         popupShown = true;
         $('#threeinone').removeClass('inactive-popup');
-
-        // Scroll to the desired section based on the button clicked
-        const buttonId = $(this).attr('id');
-        const sectionId = `${buttonId}-section`;
-        scrollToSection(sectionId);
-      }.bind(this)); // Bind the click context to the callback function
+      })
     }else{
       $('#threeinone').toggleClass('inactive-popup');
     }
-  });
-
-  // Function to scroll to a section by its ID
-  function scrollToSection(sectionId) {
-    $('html, body').animate({
-      scrollTop: $(`#${sectionId}`).offset().top
-    }, 500); // Adjust the scrolling speed as needed
-  }
+  })
 });
+// $(document).ready(function() {
+//   const popUpContainer = $('#threeInOne-popup-container');
+//   let popupShown = false;
+
+//   $('.popup-button').click(function(){
+//     if(!popupShown){
+//       $.get('../Views/threeInOnePage.html', function(data){
+//         popUpContainer.html(data);
+//         popupShown = true;
+//         $('#threeinone').removeClass('inactive-popup');
+
+//         const buttonId = $(this).attr('id');
+//         const sectionId = `${buttonId}-section`;
+//         scrollToSection(sectionId);
+//       }.bind(this));
+//     }else{
+//       $('#threeinone').toggleClass('inactive-popup');
+//     }
+//   });
+
+//   function scrollToSection(sectionId) {
+//     const scrollTopPosition = $(`#${sectionId}`).offset().top - popUpContainer.offset().top;
+//     console.log(scrollTopPosition);
+//     console.log($(`#${sectionId}`).offset().top);
+//     console.log($('#threeInOne-popup-container'))
+//     $('#threeInOne-popup-container').animate({
+//       scrollTop: $(`#${sectionId}`).offset().top
+//     }, 500); 
+//   }
+// });
