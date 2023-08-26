fa2a5a6d27280bc901818a82454cab9c6848d78a
AmaraCela <amaracela@gmail.com>
2023-08-26 14:46:17 +0200

    bug fixes in the individal tour page

--- Controlls/temp.js
+++ Controlls/temp.js
@@ -110,7 +110,7 @@ window.addEventListener('scroll', ()=>{
 
 
 // --------------------------DISABLE BUTTON------------------
-if(window.location.href.includes("/Views/ourStaff1.html") || window.location.href.includes("/Views/mapSelect.html") || window.location.href.includes("ourWork.html")){
+if(window.location.href.includes("/Views/ourStaff1.html") || window.location.href.includes("/Views/mapSelect.html") || window.location.href.includes("ourWork.html") || window.location.href.includes("individualTour.html")){
   summerWinterTogglebtn.classList.add('disabled');
 }
 
